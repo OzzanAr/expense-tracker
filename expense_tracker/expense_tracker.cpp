8b66b7236237804ed4b9744462e94767dba29f74
OzzanAr <ozan.arif@student.um.si>
2025-03-29 02:00:17 +0100

    Minor updates

--- expense_tracker/expense_tracker.cpp
+++ expense_tracker/expense_tracker.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <string>
+#include <Windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool isRunning = true;
+    std::string inputCommand;
+    std::string description;
+    int amount;
 
+
+    while (isRunning) {
+		std::cin >> inputCommand;
+
+        if (inputCommand == "exit") {
+            std::cout << "Shutting down...";
+            isRunning = false;
+            break;
+        }
+
+        if (inputCommand.substr(0, 15) != "expanse-tracker") {
+            std::cout << "Incorrect format for command.\nCorrect format is expanse-tracker.\n";
+        }
+
+        std::cout << inputCommand.substr(0, 15) << std::endl;
+    }
 
 
     return 0;
