aae26491c5e5d1b6825ea620ac4932a3dd07c9e0
OzzanAr <ozan.arif@student.um.si>
2025-03-31 00:14:19 +0200

    Added class and basic functionality.

--- expense_tracker/expense_tracker.cpp
+++ expense_tracker/expense_tracker.cpp
@@ -2,30 +2,71 @@
 #include <string>
 #include <Windows.h>
 
+//enum Type{
+//	ADD,
+//	LIST,
+//};
+
+class Expense {
+public:
+	int id;
+	int amount;
+	std::string description;
+
+	void AddExpense(std::string input) {
+		this->id = 5;
+		this->amount = stoi(input);
+	}
+
+	void PrintExpense() {
+		std::cout << "ID" << " " << "Amount" << std::endl;
+		std::cout << id << " " << amount << std::endl;
+	}
+};
+
 int main()
 {
-    bool isRunning = true;
-    std::string inputCommand;
-    std::string description;
-    int amount;
+	bool isRunning = true;
+	bool isExpense = false;
+	bool isAdd = false;
 
+	// Type inputType;
+	Expense userExpense;
 
-    while (isRunning) {
+	std::string inputCommand;
+	std::string description;
+
+	std::string temp;
+
+	while (isRunning) {
 		std::cin >> inputCommand;
 
-        if (inputCommand == "exit") {
-            std::cout << "Shutting down...";
-            isRunning = false;
-            break;
-        }
+		if (inputCommand == "exit") {
+			std::cout << "Shutting down...";
+			isRunning = false;
+			break;
+		}
 
-        if (inputCommand.substr(0, 15) != "expanse-tracker") {
-            std::cout << "Incorrect format for command.\nCorrect format is expanse-tracker.\n";
-        }
+		if (!isExpense && inputCommand != "expense-tracker") {
+			std::cout << "Incorrect format for command.\nCorrect format is expanse-tracker.\n";
+			isExpense = true;
+		}
 
-        std::cout << inputCommand.substr(0, 15) << std::endl;
-    }
+		if (isAdd) {
+			userExpense.AddExpense(inputCommand);
+			isAdd = false;
+		}
 
+		if (inputCommand == "add"){ 
+			std::cout << "Added expense\n";
+			// inputType = ADD;
+			isAdd = true;
+		}
 
-    return 0;
+		if (inputCommand == "list"){ 
+			userExpense.PrintExpense();
+		}
+	}
+
+	return 0;
 }
